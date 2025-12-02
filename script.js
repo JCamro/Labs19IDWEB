7dd3a46ae155e7f47b4ea0466d0e4e5bce312c24
JCamro <cameroelguerajosue@gmail.com>
2025-12-01 19:02:11 -0500

    Agregando ejercicios

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ async function obtenerPokemon(id) {
         return data;
 
     } catch (error) {
-        return error;
+        throw error;
     }
 } 
 
@@ -122,42 +122,179 @@ const btn9 = document.getElementById("btn9");
 const entradaId = document.getElementById("entrada9");
 const div9 = document.getElementById("ejercicio9");
 
-btn9.addEventListener("click", () => {
+btn9.addEventListener("click", async () => {
     div9.innerHTML = "";
     try {
-        const pokemon = obtenerPokemon("1");
+        
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${entradaId.value}`);
+
+        if (!res.ok) {
+            throw new Error("Error en la solicitud: "+res.status);
+        }
+
+        const data = await res.json();
+        
+        //Creando imagen
+        const imagen = document.createElement("img");
+        imagen.src = data.sprites.front_default; 
+        imagen.alt = "Imagen Pokemon";
+
+        //Creando Nombred.heigh
+        const nombre = document.createElement("p");
+        nombre.textContent = "Nombre: "+data.name;
+        //Creando id
+        const id = document.createElement("p");
+        id.textContent = "ID: "+data.id;
+        //Creando peso
+        const peso = document.createElement("p");
+        peso.textContent = "Peso: "+data.weight;
+        //Creando altura
+        const altura = document.createElement("p");
+        altura.textContent = "Altura: "+data.height;
+        
+        div9.appendChild(nombre);
+        div9.appendChild(id);
+        div9.appendChild(imagen);
+        div9.appendChild(peso);
+        div9.appendChild(altura);
+        
+    } catch (error) {
+        //Manejando entrada no valida
+        const manejo = document.createElement("p");
+        manejo.textContent = "No se pudo encontrar este Pokemon!!";
+        div9.appendChild(manejo);
+    }
+});
+
+
+async function cargarTarjeta(poke, div) {
+    const card_content = document.getElementById(div);
+    card_content.style.display = "grid";
+    card_content.style.gridTemplateColumns = "1fr 1fr 1fr 1fr";
+    const card = document.createElement("div");
+    
+    card.style.backgroundColor = "green";
+    card.style.border = "solid 1px black";
+    card.style.margin = "10px";
+    card.style.padding = "5px";
+
+    try {
+        const pokemon = await obtenerPokemon(poke);
 
         //Creando imagen
         const imagen = document.createElement("img");
         imagen.src = pokemon.sprites.front_default; 
         imagen.alt = "Imagen Pokemon";
-        imagen.width = "200px";
 
-        //Creando Nombre
+        //Creando Nombred.heigh
         const nombre = document.createElement("p");
         nombre.textContent = "Nombre: "+pokemon.name;
         //Creando id
         const id = document.createElement("p");
         id.textContent = "ID: "+pokemon.id;
-        //Creando peso
-        const peso = document.createElement("p");
-        peso.textContent = "Peso: "+pokemon.weight;
-        //Creando altura
-        const altura = document.createElement("p");
-        altura.textContent = "Altura: "+pokemon.height;
+
+        card.appendChild(nombre);
+        card.appendChild(id);
+        card.appendChild(imagen);
+
+        card_content.appendChild(card);
+
+    } catch (error) {
+        //Manejando entrada no valida
+        const manejo = document.createElement("p");
+        manejo.textContent = error;
+        card.appendChild(manejo);
+        card_content.appendChild(card);
+    }
+}
+
+for (let index = 0; index < 10; index++) {
+    cargarTarjeta((index+1), "ejercicio10");
+}
+
+
+const btn11 = document.getElementById("btn11");
+const entradaPoke = document.getElementById("entrada11");
+const div11 = document.getElementById("ejercicio11");
+
+btn11.addEventListener("click", async () => {
+    div11.innerHTML = "";
+    try {
         
-        div9.appendChild(imagen)
-        div9.appendChild(nombre)
-        div9.appendChild(id)
-        div9.appendChild(peso)
-        div9.appendChild(altura)
+        const pokemon = await obtenerPokemon(entradaPoke.value);
+        
+        //Creando imagen
+        const imagen = document.createElement("img");
+        imagen.src = pokemon.sprites.front_default; 
+        imagen.alt = "Imagen Pokemon";
+
+        //Creando Nombred.heigh
+        const nombre = document.createElement("p");
+        nombre.textContent = "Nombre: "+pokemon.name;
+        //Creando id
+        const id = document.createElement("p");
+        id.textContent = "ID: "+pokemon.id;
+        //Creando Tipos
+        const tipos = document.createElement("ul");
+
+        for (const tipo of pokemon.types) {
+            const li = document.createElement("li");
+            li.textContent = tipo.type.name;
+            tipos.appendChild(li);
+        }
+        
+        div11.appendChild(imagen);
+        div11.appendChild(nombre);
+        div11.appendChild(id);
+        div11.appendChild(tipos);
         
     } catch (error) {
         //Manejando entrada no valida
-        const peso = document.createElement("p");
-        peso.textContent = "No se encontro en la base de datos ese Pokemon!";
-        div9.appendChild(peso);
-        
+        const manejo = document.createElement("p");
+        manejo.textContent = "No se pudo encontrar este Pokemon!!";
+        div11.appendChild(manejo);
     }
+});
 
+const btn12 = document.getElementById("btn13"); 
+const entradaPoke12 = document.getElementById("entrada11");
+const div12 = document.getElementById("ejercicio11");
+
+btn12.addEventListener("click", async () => {
+    div12.innerHTML = "";
+    try {
+        
+        const pokemon = await obtenerPokemon(entradaPoke12.value);
+        
+        //Creando imagen
+        const imagen = document.createElement("img");
+        imagen.src = pokemon.sprites.front_default; 
+        imagen.alt = "Imagen Pokemon";
+
+        //Creando Nombred.heigh
+        const nombre = document.createElement("p");
+        nombre.textContent = "Nombre: "+pokemon.name;
+        //Creando id
+        const id = document.createElement("p");
+        id.textContent = "ID: "+pokemon.id;
+        //Creando Tipos
+        const tipos = document.createElement("ul");
+
+        for (const tipo of pokemon.types) {
+            const li = document.createElement("li");
+            li.textContent = tipo.type.name;
+            tipos.appendChild(li);
+        }
+        
+        div11.appendChild(imagen);
+        div11.appendChild(nombre);
+        div11.appendChild(id);
+        div11.appendChild(tipos);
+        
+    } catch (error) {
+        //Manejando entrada no valida
+        const manejo = document.createElement("p");
+        manejo.textContent = "No se pudo encontrar este Pokemon!!";
+        div11.appendChild(manejo);
+    }
 });
